a61352a5518bce3399cd0a6b32eb84be7622ec34
Tino7grass <tino.grass@gmail.com>
2026-07-09 21:08:39 +0200

    fix: Audit-Export liest aus context-spezifischem Store

--- netlify/functions/audit-export.js
+++ netlify/functions/audit-export.js
@@ -1,5 +1,6 @@
 // netlify/functions/audit-export.js
 const { getStore } = require("@netlify/blobs");
+const { contextualStoreName } = require("./lib/store-name");
 
 exports.handler = async (event) => {
   const headers = {
@@ -16,7 +17,7 @@ exports.handler = async (event) => {
 
   try {
     const store = getStore({
-      name: "clearifyer-audit-log",
+      name: contextualStoreName("clearifyer-audit-log"),
       siteID: process.env.NETLIFY_SITE_ID || "24815739-0429-4422-8273-c4309c9b6753",
       token: process.env.NETLIFY_TOKEN
     });
